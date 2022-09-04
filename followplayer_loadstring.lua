6adef00e3f88734928110731c4587c74f4802102
globaldoeslua <112278719+globaldoeslua@users.noreply.github.com>
2022-09-04 22:45:03 +0200

    Create followplayer_loadstring.lua

---
+++ followplayer_loadstring.lua
@@ -0,0 +1,4 @@
+getgenv().Target = game.Players.freakyghoul2937 -- the target, the "TARGET" needs to be the target's full username. example: game.Players.grimdhcalt9
+getgenv().Time = 15 -- how long you wanna follow the target
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/globaldoeslua/scripts/main/followplayer_OBFUSCATED.lua'))()
